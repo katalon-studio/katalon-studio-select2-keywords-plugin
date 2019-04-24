d44480449820e83850ec9ee164f0ad44419d0af8
Trung Lu <trunglu@kms-technology.com>
2019-04-25 05:55:25 +0700

    add jar file

---
+++ Libs/CustomKeywords.groovy
@@ -0,0 +1,79 @@
+
+/**
+ * This class is generated automatically by Katalon Studio and should not be modified or deleted.
+ */
+
+import com.kms.katalon.core.testobject.TestObject
+
+import java.lang.String
+
+import java.util.List
+
+
+def static "com.kms.katalon.keyword.select2.Utils.clickOnSelect2"(
+    	TestObject to	) {
+    (new com.kms.katalon.keyword.select2.Utils()).clickOnSelect2(
+        	to)
+}
+
+def static "com.kms.katalon.keyword.select2.Utils.findContainer"(
+    	TestObject to	) {
+    (new com.kms.katalon.keyword.select2.Utils()).findContainer(
+        	to)
+}
+
+def static "com.kms.katalon.keyword.select2.Utils.selectResult"(
+    	String option	
+     , 	String subContainerOpenClass	) {
+    (new com.kms.katalon.keyword.select2.Utils()).selectResult(
+        	option
+         , 	subContainerOpenClass)
+}
+
+def static "com.kms.katalon.keyword.select2.Utils.enterText"(
+    	String option	) {
+    (new com.kms.katalon.keyword.select2.Utils()).enterText(
+        	option)
+}
+
+def static "com.kms.katalon.keyword.select2.Select2.selectOptionByLabel"(
+    	TestObject to	
+     , 	String option	) {
+    (new com.kms.katalon.keyword.select2.Select2()).selectOptionByLabel(
+        	to
+         , 	option)
+}
+
+def static "com.kms.katalon.keyword.select2.Select2.selectManyOptionsByLabel"(
+    	TestObject to	
+     , 	java.util.List<String> options	) {
+    (new com.kms.katalon.keyword.select2.Select2()).selectManyOptionsByLabel(
+        	to
+         , 	options)
+}
+
+def static "com.kms.katalon.keyword.select2.Select2.getSelectedOptionsLabel"(
+    	TestObject to	) {
+    (new com.kms.katalon.keyword.select2.Select2()).getSelectedOptionsLabel(
+        	to)
+}
+
+def static "com.kms.katalon.keyword.select2.Select2.getSelectedOptionsList"(
+    	TestObject to	) {
+    (new com.kms.katalon.keyword.select2.Select2()).getSelectedOptionsList(
+        	to)
+}
+
+def static "com.kms.katalon.keyword.select2.Select2.getAllOptionsLabel"(
+    	TestObject to	) {
+    (new com.kms.katalon.keyword.select2.Select2()).getAllOptionsLabel(
+        	to)
+}
+
+def static "com.kms.katalon.keyword.select2.Select2.removeOptions"(
+    	TestObject to	
+     , 	java.util.List<String> options	) {
+    (new com.kms.katalon.keyword.select2.Select2()).removeOptions(
+        	to
+         , 	options)
+}
